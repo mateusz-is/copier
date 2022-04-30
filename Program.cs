671e315914c9982ce43159e9647d12e394371fd3
Mateusz <mateusz.tobiasz@isprzet.pl>
2022-04-30 12:11:50 +0200

    Wdrozenie podstawowych plików

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Copier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
